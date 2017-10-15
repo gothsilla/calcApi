83de325e0f6a62a4024b00325f324b2eaeab6e23
Skúli Arnlaugsson <skuli.arnlaugsson@gmail.com>
2017-10-15 15:10:07 +0000

    Multiple numbers

--- src/main/java/is/ru/hugb/calc/StringCalculator.java
+++ src/main/java/is/ru/hugb/calc/StringCalculator.java
@@ -4,13 +4,17 @@ import java.util.stream.Stream;
 
 public class StringCalculator {
     public int add(String input) {
+        int sum = 0;
         if (input.equals("")) {
             return 0;
         }
         else if (input.contains(",")) {
             Integer[] numbers = Stream.of(input.split(","))
                 .map(Integer::valueOf).toArray(Integer[]::new);
-            return numbers[0] + numbers[1];
+            for (int number : numbers) {
+                sum += number;
+            }
+            return sum;
         }
         return 1;
     }
